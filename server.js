cc3ba3411f7702d26502371a16d73d26af1e5b70
jthineshanand <jthineshanand@gmail.com>
2018-03-14 10:34:31 +0530

    DB connect

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var User = require('./models/users')
 
+mongoose.connect('mongodb://cja:cja@ds117878.mlab.com:17878/ecommerce',function(err){
+  if(err) throw err;
+  else console.log('Database Connected');
+})
 
 var app = express();
 app.use(morgan('dev'));
